fefc5f0c2885c4e570c9699a470e7437a8413212
Paul Barnes <paulba71@hotmail.com>
2025-03-12 08:39:57 +0000

    Initial Commit

---
+++ KeyInfo/KeyInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyInfoApp.swift
+//  KeyInfo
+//
+//  Created by Paul Barnes on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct KeyInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyInfoTests/KeyInfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeyInfoTests.swift
+//  KeyInfoTests
+//
+//  Created by Paul Barnes on 12/03/2025.
+//
+
+import Testing
+@testable import KeyInfo
+
+struct KeyInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
